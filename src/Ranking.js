5f4deac5c81de8e1aebcd247591660abf4a94af8
Przemysław Moskała <przemyslaw.moskala@synerise.com>
2019-06-11 22:49:24 +0200

    Fix ranking

--- src/Ranking.js
+++ src/Ranking.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Slider from "react-slick";
+import SideBar from "./HomeSidebar";
 
 const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
 const RankingEntry = (props) => {
@@ -39,16 +40,18 @@ class Ranking extends React.Component {
 
         return(
             <div className='body'>
+                <SideBar/>
                 <div className='content-container justified'>
                     <div className='ranking-table-wrapper justified center-alligned column-direction'>
                         <RankingEntry position={"1"} avatar={"http://lorempixel.com/32/31"} name={"Anna Nowak"} title={"Clean Queen"} score={540}/>
                         <RankingEntry position={"2"}  avatar={"http://lorempixel.com/32/32"} name={"Jan Kowalski"} title={"Clean King"} score={400}/>
                         <RankingEntry position={"3"} avatar={"http://lorempixel.com/32/33"} name={"Agnieszka Kowalska"} title={"Clean Queen"} score={250}/>
                         <RankingEntry position={"4"}  avatar={"http://lorempixel.com/33/32"} name={"Adam Kowalski"} title={"Clean King"} score={200}/>
-                        <RankingEntry position={"5"}  avatar={"http://lorempixel.com/33/32"} name={"Janina Nowak"} title={""} score={10}/>
+                        <RankingEntry position={"5"}  avatar={"http://lorempixel.com/33/33"} name={"Janina Nowak"} title={""} score={10}/>
                     </div>
 
                     <div className='legend-wrapper'>
+                        <h1 className="legend-title">Legend</h1>
 
                             <Slider {...settings}>
                               <LegendEntry title={"Clean Queen"} content={lorem}/>
